c4a8c842b382a3964ef9678ec973a3affb0ea593
lottermnn <lottermanneduardo81@gmail.com>
2023-01-21 17:46:12 -0300

    Olds as new's

--- script.js
+++ script.js
@@ -1,61 +1,27 @@
-const form = document.querySelector("form")
-const newHabit = new NLWSetup(form)
-const addHabit = document.querySelector("header .add-habit")
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-addHabit.addEventListener("click", add)
+button.addEventListener("click", add)
 form.addEventListener("change", save)
-
-const button = document.querySelector(".add-habit")
-const modal = document.querySelector("dialog")
-const buttonClose = document.querySelector("dialog button")
-
-addHabit.onclick = function () {
-  modal.showModal()
-}
-
-buttonClose.onclick = function () {
-  modal.close()
-}
 
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
-  const dayExists = newHabit.dayExists(today)
+  const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
-    document.getElementById("confirm-habit").innerHTML = "Já está registrado"
+    alert("Dia já incluso 🔴")
     return
   }
 
-  document.getElementById("confirm-habit").innerHTML =
-    "Seu dia foi registrado com sucesso"
-
-  newHabit.addDay(today)
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
 
 function save() {
-  localStorage.setItem("controltoday@habits", JSON.stringify(newHabit.data)) ||
-    {}
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem("controltoday@habits"))
-newHabit.setData(data)
-newHabit.load()
-
-var today = new Date()
-var dd = String(today.getDate()).padStart(2, "0")
-var mm = String(today.getMonth() + 1).padStart(2, "0") //January is 0!
-var yyyy = today.getFullYear()
-var daysOfWeek = [
-  "Domingo",
-  "Segunda-feira",
-  "Terça-feira",
-  "Quarta-feira",
-  "Quinta-feira",
-  "Sexta-feira",
-  "Sábado",
-]
-var dayOfWeek = daysOfWeek[today.getDay()]
-
-today = dd + "/" + mm
-document.getElementById("demo").innerHTML = today
-document.getElementById("dayof").innerHTML = dayOfWeek
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
